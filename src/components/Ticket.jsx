ceb790e0d369c66efe6f893d71043ad3df8d9174
James Cho <hwisoocho@gmail.com>
2019-02-12 10:33:13 -0800

    add Linting with ESLint

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 function Ticket(props){
   return (
     <div>
-        <h3>{props.location} - {props.names}</h3>
+      <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Ticket from './Ticket';
 function TicketList(){
   return (

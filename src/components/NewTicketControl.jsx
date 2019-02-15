93f7831e4e45356366bf5b519beaa6a67842b260
James Cho <hwisoocho@gmail.com>
2019-02-14 16:13:30 -0800

    Lifting State

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from "prop-types";
 
 class NewTicketControl extends React.Component {
     constructor(props) {
@@ -18,7 +19,7 @@ class NewTicketControl extends React.Component {
     render(){
         let currentlyVisibleContent = null;
         if (this.state.formVisibleOnPage){
-          currentlyVisibleContent = <NewTicketForm />;
+          currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
         } else {
           currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
         }
@@ -29,5 +30,9 @@ class NewTicketControl extends React.Component {
     );
   }
 }
+
+NewTicketControl.propTypes = {
+    onNewTicketCreation: PropTypes.func
+  };
 
 export default NewTicketControl;
